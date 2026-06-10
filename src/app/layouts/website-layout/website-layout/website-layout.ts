7bd546aa4b4376820460a2f3c896c7763b7ecc5b
amihub <amihub2026@gmail.com>
2026-06-10 11:22:07 +0530

    Initial Angular project setup

---
+++ src/app/layouts/website-layout/website-layout/website-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-layout',
+  imports: [],
+  templateUrl: './website-layout.html',
+  styleUrl: './website-layout.css',
+})
+export class WebsiteLayout {}
