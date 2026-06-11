575d96cde48619c65ab521ef3caaff1231d94fff
amihub <amihub2026@gmail.com>
2026-06-11 11:37:13 +0530

    Update home page design

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  @ViewChild('productsContainer')
+  productsContainer!: ElementRef<HTMLDivElement>;
+
+  scrollLeft(): void {
+    this.productsContainer.nativeElement.scrollBy({
+      left: -350,
+      behavior: 'smooth'
+    });
+  }
+
+  scrollRight(): void {
+    this.productsContainer.nativeElement.scrollBy({
+      left: 350,
+      behavior: 'smooth'
+    });
+  }
+
+}
