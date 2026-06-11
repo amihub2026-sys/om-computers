b2f96aaec540b1777993fae518b3870a56d2b693
amihub <amihub2026@gmail.com>
2026-06-11 17:48:52 +0530

    add-product

--- src/app/admin/manage-products/manage-products.ts
+++ src/app/admin/manage-products/manage-products.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-manage-products',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './manage-products.html',
   styleUrl: './manage-products.css',
 })
