568b693b8cdf2c1e7cef1207fcdb52b3f6820ec5
amihub <amihub2026@gmail.com>
2026-06-11 18:13:42 +0530

    manage-bookings

--- src/app/admin/manage-bookings/manage-bookings.ts
+++ src/app/admin/manage-bookings/manage-bookings.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-manage-bookings',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './manage-bookings.html',
   styleUrl: './manage-bookings.css',
 })
