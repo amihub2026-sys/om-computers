c826b83f32c99b4333d378591bd7293f31d422bc
amihub <amihub2026@gmail.com>
2026-06-11 17:31:39 +0530

    manage-products

--- src/app/admin/manage-products/manage-products.ts
+++ src/app/admin/manage-products/manage-products.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-manage-products',
+  standalone: true,
   imports: [],
   templateUrl: './manage-products.html',
   styleUrl: './manage-products.css',
