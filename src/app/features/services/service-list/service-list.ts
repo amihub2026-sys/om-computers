b8c1a9ba0f825714d0c7470b16e7b49a0b4cf4ff
AMI HUB <amihub2026@gmail.com>
2026-06-15 15:20:52 +0530

    Update service filter design and mobile UI

--- src/app/features/services/service-list/service-list.ts
+++ src/app/features/services/service-list/service-list.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './service-list.html',
   styleUrl: './service-list.css',
 })
-export class ServiceList {}
+export class ServiceList {
+
+  showFilters = false;
+
+  filters = {
+    laptopRepair: false,
+    desktopRepair: false,
+    printerService: false,
+    networking: false,
+    dataRecovery: false,
+    doorstep: false,
+    inShop: false,
+    remote: false,
+    sameDay: false,
+    support247: false,
+    business: false,
+  };
+
+  resetFilters() {
+    Object.keys(this.filters).forEach((key) => {
+      this.filters[key as keyof typeof this.filters] = false;
+    });
+  }
+
+  applyFilters() {
+    this.showFilters = false;
+  }
+}
